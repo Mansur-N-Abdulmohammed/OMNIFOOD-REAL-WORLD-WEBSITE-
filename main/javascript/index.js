152e3d45399d519919e6327dd5321026f5408eab
MansurSharazoori <Xpert.Web.911@gmail.com>
2025-09-15 01:47:02 +0300

    network section

--- main/javascript/index.js
+++ main/javascript/index.js
@@ -8,14 +8,40 @@ date.textContent = new Date().getFullYear();
 var btn = document.querySelector(".hidden");
 var headerEL = document.querySelector(".header");
 // var menuEl = document.querySelector(".menu-section");
-// const item = document.querySelector(".list-item");
 
 btn.addEventListener("click", function () {
   headerEL.classList.toggle("nav-open");
-
-  if ('headerEL.classList.toggle("nav-open")' == false) {
-    console.log("it is opening");
-  } else {
-    console.log("it is closing");
-  }
 });
+
+// closing the navigation side bar through the links
+const item = document.querySelectorAll(".list-item:link");
+
+item.forEach(function (link) {
+  link.addEventListener("click", function () {
+    console.log(link);
+    headerEL.classList.toggle("nav-open");
+  });
+});
+
+///////////////////////////////////////////////////////////
+// Sticky navigation
+
+///////////////////////////////////////////////////////////
+// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
